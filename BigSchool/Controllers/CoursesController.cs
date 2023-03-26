61a5ba8544304eb9b0645e1f096d38b3acea7521
Nguyen Huynh Tieu Yen <nguyenhuynhtieuyen2909@gmail.com>
2023-03-26 23:04:32 +0700

    hoan thanh tuan 2

--- BigSchool/Controllers/CoursesController.cs
+++ BigSchool/Controllers/CoursesController.cs
@@ -1,11 +1,14 @@
 ﻿using BigSchool.Models;
 using BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+namespace BigSchool.Controllers
+{
 
     public class CoursesController : Controller
     {
@@ -15,13 +18,41 @@ using System.Web.Mvc;
         {
             _dbContext = new ApplicationDbContext();
         }
+        [Authorize]
+
         public ActionResult Create()
         {
-        var viewModel = new CourseViewModel
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModel viewModel)
         {
-            Categories = _dbContext.Categories.ToList()
-        };
-        
-        return View(viewModel);
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LectureID = User.Identity.GetUserId(),
+                Datetime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+
+            };
+            _dbContext.Course.Add(course);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+
+        }
     }
 }
